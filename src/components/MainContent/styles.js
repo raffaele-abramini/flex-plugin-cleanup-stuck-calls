44e44948ca9b4a1c3965177d52fcf7060e127de1
rabramini <rabramini@twilio.com>
2019-09-25 14:48:02 +0100

    Clean

--- src/components/MainContent/styles.js
+++
@@ -1,6 +0,0 @@
-import { styled } from '@twilio/flex-ui'
-
-export const MainContainer = styled('div')`
-  display: flex;
-  align-items: center;
-`
--- src/index.js
+++
@@ -1,4 +0,0 @@
-import * as FlexPlugin from 'flex-plugin';
-import FixCallRaceConditionPlugin from './FixCallRaceConditionPlugin';
-
-FlexPlugin.loadPlugin(FixCallRaceConditionPlugin);
